cc28cd80b8bbcd27ad35a0ba88b9465eed12909b
thecreatorzx <saadyesican@gmail.com>
2026-03-30 06:03:05 +0530

    usestate not used issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import StockDashboard from "./components/StockDashboard";
 import TopStrip from "./components/TopStrip";
 import "./App.css";
